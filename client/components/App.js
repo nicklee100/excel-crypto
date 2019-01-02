bfdd0121d93f7696f28ae47e5a55d50ee3bf3130
Nick Lee <nicklee100@gmail.com>
2019-01-01 21:49:48 -0500

    initial commit

---
+++ client/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class CurrencyChooser extends React.Component {
+  constructor(){
+
+  }
+
+  render() {
+
+    return (
+      <div className="currency-chooser">
+        <h1>Currency</h1>
+      </div>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  app: path.join(__dirname, 'client/'),
+}
+
+module.exports = {
+  entry: PATHS.app,
+  output: {
+    path: path.resolve(__dirname, './build/public/'),
+    filename: 'index_bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'index.template.ejs',
+
+  })],
+  module:{
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', ]
+          }
+        }
+      }
+    ]
+  }
+}
