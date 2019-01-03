f605aa1c526ec309921ca5ebd9ed2aaefbbf8e2f
Nick Lee <nicklee100@gmail.com>
2019-01-02 22:46:19 -0500

    added webpack rules for css

--- build/public/index_bundle.js
+++ build/public/index_bundle.js
@@ -24083,8 +24083,6 @@ function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) ===
 
 function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
@@ -24098,9 +24096,11 @@ function (_React$Component) {
   _inherits(CurrencyChooser, _React$Component);
 
   function CurrencyChooser() {
+    var _this;
+
     _classCallCheck(this, CurrencyChooser);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(CurrencyChooser).apply(this, arguments));
+    return _possibleConstructorReturn(_this);
   }
 
   _createClass(CurrencyChooser, [{
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,13 @@ module.exports = {
   module:{
     rules: [
       {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
